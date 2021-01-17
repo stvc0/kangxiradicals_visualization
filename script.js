6bda8a1cc77c2146b57c48837b0a4060f2705512
Ern Tan <60453951+erntan@users.noreply.github.com>
2021-01-17 15:56:42 -0500

    Updated requestURl

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var characters = JSON.parse(data);
+// var characters = JSON.parse(data);
 
 const numRadicals = 214;    // The number of Kangxi radicals.
 const maxStroke = 17;       // The maximum number of strokes a radical can have.
@@ -8,25 +8,32 @@ const maxRowSize = 16;
 var radTable = document.getElementById("radicals-table");
 
 /* Get the json file. */
-let requestURL = "https://github.com/erntan/hsk/blob/master/data.json";
+let requestURL = "https://raw.githubusercontent.com/erntan/hanziradicals_visualization/master/data.json";
 let request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = "json";
 request.send();
 
-request.onload = function () {
-  const characters = request.response;
-  createRadicalsTable(characters);
-}
+
+// request.onload = function () {
+const charactersJSON = request.response;
+const characters = JSON.parse(charactersJSON);
+  // createRadicalsTable(charactersJSON);
+const printThis = document.createElement("p");
+printThis.textContent = characters;
+radTable.appendChild(printThis);
+// }
+
+/* Need: rads["radicals"][i] */
 
 /* Create and populate the radicals table. */
-function createRadicalsTable(obj) {
-  const rads = obj["radicals"];
+// function createRadicalsTable(obj) {
+  // const radsJSON = charactersJSON["radicals"];
 
-  for (let i = 1; i <= numRadicals; i++) {
-    var radDiv = document.createElement("div");
-    // radDiv.textContent = rads[i];
+  for (let i = 0; i < numRadicals; i++) {
+    const radDiv = document.createElement("div");
     radDiv.textContent = i;
+    // radDiv.textContent = radsJSON[i].id;
     radTable.appendChild(radDiv);
   }
 // }
