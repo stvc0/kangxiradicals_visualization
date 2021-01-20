1180b2e87003e9c32da1dbcf865754c00890e4ea
Ern Tan <60453951+erntan@users.noreply.github.com>
2021-01-19 19:05:13 -0500

    Create divs for strokeCts

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const buttonHistStroke = document.getElementById("button-histogram-stroke");
 const buttonHistFreq = document.getElementById("button-histogram-frequency");
 const buttonChunk = document.getElementById("button-chunk");
 
-var tableType = chart;
+// var tableType = chart;
 
 /* Get the json file. */
 let requestURL = "https://raw.githubusercontent.com/erntan/kangxiradicals_visualization/master/data.json";
@@ -24,21 +24,40 @@ request.onload = function () {
   createRadicalsTable(charactersJSON);
 }
 
+/* Create divs for the number of strokes */
+function createStrokeDivs() {
+  for (let i = 1; i <= maxStroke; i++) {
+    const strokeDiv = document.createElement("div");
+    strokeDiv.className = "stroke-div";
+    strokeDiv.id = "stroke-div-" + i;
+    radTable.appendChild(strokeDiv);
+  }
+}
+
 /* Create and populate the radicals table. */
 function createRadicalsTable(obj) {
   const radsJSON = obj["radicals"];
 
+  createStrokeDivs();
+
   for (let i = 0; i < numRadicals; i++) {
     const radDiv = document.createElement("div");
-    radDiv.textContent = radsJSON[i].character;
-    radTable.appendChild(radDiv);
+    const currentRad = radsJSON[i];
+    
+    radDiv.textContent = currentRad.character + currentRad.strokeCount;
+
+    const strokeCount = currentRad.strokeCount;
+    const strokeDiv = document.getElementById( "stroke-div-" + strokeCount);
+    strokeDiv.appendChild(radDiv);
   }
 }
 
+
+
 function createStrokeHistogram() {
-  
+
 }
 
 
 /** Add event listeners */
-buttonHistFreq.addEventListener("click", createStrokeHistogram());
+// buttonHistFreq.addEventListener("click", createStrokeHistogram());
